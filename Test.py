3c913170a7e39dbf4393994e58ca34dba7a9b044
ebeauchant <beauchant509@gmail.com>
2023-07-13 19:35:32 -0400

    Update

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
-# 30. Convertir un nombre binaire en décimal.
+# 30. Convertir un nombre binaire en décimal..
 nombre_binaire = input("Entrez un nombre binaire: ")
 nombre_decimal = int(nombre_binaire, 2)
 print(f"Le nombre binaire {nombre_binaire} donne {nombre_decimal} en décimal.")
